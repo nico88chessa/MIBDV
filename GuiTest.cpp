ec1abf01f0a88086d574f298a20920a5b5c205b2
Nicola Chessa <n.chessa@dvoptic.com>
2018-10-29 22:13:13 +0100

    aggiunto font Roboto; aggiornamento CMakeLists.txt per errore gestione file .qrc; modificato qss;

--- GuiTest.cpp
+++ GuiTest.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QFontDatabase>
 
 #include <gui/ui/MainWindow.hpp>
 
@@ -12,6 +13,25 @@ int main(MAYBE_UNUSED int argc, MAYBE_UNUSED char** argv) {
     QCoreApplication::setOrganizationName(PROGRAM_NAMESPACE::ORGANIZATION);
     QCoreApplication::setApplicationName(PROGRAM_NAMESPACE::APPLICATION_NAME);
     QCoreApplication::setApplicationVersion(PROGRAM_NAMESPACE::APPLICATION_VERSION);
+
+    QFile file(":/fonts/Roboto-Black.ttf");
+    qDebug() << file.exists();
+
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    int id = 0;
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Black");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-BlackItalic");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Bold");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-BoldItalic");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Italic");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Light");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-LightItalic");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Medium");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-MediumItalic");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Regular");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-Thin");
+    id = QFontDatabase::addApplicationFont(":/fonts/Roboto-ThinItalic");
 
     MainWindow mainWindow;
     mainWindow.show();
