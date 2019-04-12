a1ea427e8b12244f003c7a8d2fe440bbdb30eddf
Nicola Chessa <n.chessa@dvoptic.com>
2019-04-12 15:27:01 +0200

    aggiornamento librerie ipgMarkingLibraryWrapper v1.1.1.0 (dll e lib sono compilate in Release mode); richiedono IpgMarkingLibrary v1.0.10.7667; cancellato header inutili (da testare; teoricamente utilizzati solo da VisualStudio per generazione dll);

--- third-party/ipg-marking-library-wrapper/include/PointList.h
+++ third-party/ipg-marking-library-wrapper/include/PointList.h
@@ -3,8 +3,10 @@
 #include "IpgmlDef.h"
 
 #include "Point.h"
+#include "PolygonProperties.h"
 
 #include <list>
+
 
 namespace ipg_marking_library_wrapper {
     
@@ -18,13 +20,22 @@ namespace ipg_marking_library_wrapper {
         PointList();
         PointList(std::list<Point>& points);
         PointList(PointList&& other);
+        PointList(const PolygonProperties& polygonProperties);
         ~PointList();
+        void append(const PointList& pl);
         int count() const;
-        Point element(int i);
+        Point center() const;
+        Point element(int i) const;
         void shift(float x, float y, float z);
+        void rotate(double z);
+        void rotate(double x, double y, double z);
+        void rotate(float z);
+        void rotate(float x, float y, float z);
 
         void* getManagedPtr();
         void releaseManagedPtr();
+
+        IPGMARKINGLIBRARYWRAPPER_API friend std::ostream& operator<<(std::ostream& os, const PointList& obj);
 
     };
 
